dff79c1f2531c4c1d8ddc4b7e98c9dab30567bc7
Jason Kraus <jasonk@cukerinteractive.com>
2012-02-08 17:08:47 -0800

    updated view point listing

--- cmsdemo/views.py
+++ cmsdemo/views.py
@@ -1,21 +1,14 @@
-from dockitcms.models import Collection
+from dockitcms.models import ViewPoint
 
 from dockit.views import ListView
 
 class ViewPointListingView(ListView):
-    document = Collection
+    document = ViewPoint
     template_name = 'index.html'
     
     def get_context_data(self, **kwargs):
         data = super(ViewPointListingView, self).get_context_data(**kwargs)
-        data['view_points'] = self.get_view_points(data['object_list'])
+        data['view_points'] = data['object_list']
         return data
-    
-    def get_view_points(self, collections):
-        #TODO perhaps in the future create a special indexer?
-        view_points = list()
-        for collection in collections:
-            view_points.extend(collection.view_points)
-        return view_points
 
 home = ViewPointListingView.as_view()
