bdb699f740b135f5b4e016001b08fcaac9762f3d
Jason Kraus <jasonk@cukerinteractive.com>
2012-10-29 12:35:30 -0700

    updated heroku settings

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bin/python cmsdemo/manage.py run_gunicorn --settings=heroku_settings -k gevent -b 0.0.0.0:$PORT
-#worker: bin/python eldorado/manage.py celeryd --settings=heroku_settings -E -B --loglevel=INFO
+web: python manage.py run_gunicorn --settings=cmsdemo.heroku_settings -k gevent -b 0.0.0.0:$PORT
+#worker: python manage.py celeryd --settings=cmsdemo.heroku_settings -E -B --loglevel=INFO
--- cmsdemo/heroku_settings.py
+++ cmsdemo/heroku_settings.py
@@ -18,10 +18,10 @@ if 'S3_KEY' in os.environ:
     AWS_ACCESS_KEY_ID = os.environ['S3_KEY']
     AWS_SECRET_ACCESS_KEY = os.environ['S3_SECRET']
 
-    DEFAULT_FILE_STORAGE = 'storage.UnicodeSafeS3Storage'
+    DEFAULT_FILE_STORAGE = 'cmsdemo.storage.UnicodeSafeS3Storage'
     AWS_STORAGE_BUCKET_NAME = "%s-media" % PROJECT_NAME
 
-    STATICFILES_STORAGE="storage.StaticS3FileStorage"
+    STATICFILES_STORAGE="cmsdemo.storage.StaticS3FileStorage"
     AWS_STATIC_STORAGE_BUCKET_NAME = '%s-static' % PROJECT_NAME
     STATIC_URL = "https://s3.amazonaws.com/%s/" % AWS_STATIC_STORAGE_BUCKET_NAME
     ADMIN_MEDIA_PREFIX = '%sadmin/'%STATIC_URL
@@ -59,15 +59,66 @@ if ('MEMCACHE_PASSWORD' in os.environ and
         },
     }
 
+if 'RABBITMQ_URL' in os.environ:
+    BROKER_URL = os.environ['RABBITMQ_URL']
+    CELERY_ALWAYS_EAGER = False
+
+if 'SENTRY_DSN' in os.environ:
+    SENTRY_DSN = os.environ['SENTRY_DSN']
+    if 'raven.contrib.django' not in INSTALLED_APPS:
+        INSTALLED_APPS.append('raven.contrib.django')
+
 if 'MONGOLAB_URI' in os.environ:
     pattern = re.compile(r'^mongodb://(?P<username>[\w\d]+):(?P<password>[\w\d]+)@(?P<host>.+):(?P<port>\d+)/(?P<database>[\w\d]+)')
     match = pattern.match(os.environ['MONGOLAB_URI'])
     if match:
         params = match.groupdict()
         
-        MONGO_HOST = params['host']
-        MONGO_USER = params['username']
-        MONGO_PASSWORD = params['password']
-        MONGO_PORT = int(params['port'])
-        MONGO_DB = params['database']
+        DOCKIT_BACKENDS['mongo'] = {
+            'ENGINE':'dockit.backends.mongo.backend.MongoDocumentStorage',
+            'USER':params['username'],
+            'PASSWORD':params['password'],
+            'DB':params['database'],
+            'HOST':params['host'],
+            'PORT':int(params['port']),
+        }
+        DOCKIT_INDEX_BACKENDS['mongo'] = {
+            'ENGINE':'dockit.backends.mongo.backend.MongoIndexStorage',
+            'USER':params['username'],
+            'PASSWORD':params['password'],
+            'DB':params['database'],
+            'HOST':params['host'],
+            'PORT':int(params['port']),
+        }
+        DOCKIT_BACKENDS['default'] = DOCKIT_BACKENDS['mongo']
+        DOCKIT_INDEX_BACKENDS['default'] = DOCKIT_INDEX_BACKENDS['mongo']
+
+if 'MONGOHQ_URL' in os.environ:
+    pattern = re.compile(r'^mongodb://(?P<username>[\w\d]+):(?P<password>[\w\d]+)@(?P<host>.+):(?P<port>\d+)/(?P<database>[\w\d]+)')
+    match = pattern.match(os.environ['MONGOHQ_URL'])
+    if match:
+        params = match.groupdict()
+        
+        DOCKIT_BACKENDS['mongo'] = {
+            'ENGINE':'dockit.backends.mongo.backend.MongoDocumentStorage',
+            'USER':params['username'],
+            'PASSWORD':params['password'],
+            'DB':params['database'],
+            'HOST':params['host'],
+            'PORT':int(params['port']),
+        }
+        DOCKIT_INDEX_BACKENDS['mongo'] = {
+            'ENGINE':'dockit.backends.mongo.backend.MongoIndexStorage',
+            'USER':params['username'],
+            'PASSWORD':params['password'],
+            'DB':params['database'],
+            'HOST':params['host'],
+            'PORT':int(params['port']),
+        }
+        DOCKIT_BACKENDS['default'] = DOCKIT_BACKENDS['mongo']
+        DOCKIT_INDEX_BACKENDS['default'] = DOCKIT_INDEX_BACKENDS['mongo']
+
+if 'djcelery' in INSTALLED_APPS:
+    import djcelery
+    djcelery.setup_loader()
 
